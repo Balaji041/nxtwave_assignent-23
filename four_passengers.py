7e282f2cfcd5e0cc5837572b8f2e285940e5f1b3
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-26 22:52:37 +0530

    Create four_passengers.py

---
+++ four_passengers.py
@@ -0,0 +1,11 @@
+def number_of_cars_needed(no_of_people):
+    # Complete this function
+    return round(no_of_people/4)
+no_of_people = int(input())
+# Call the number_of_cars_needed function
+print(number_of_cars_needed(no_of_people))
+
+"""
+input:11
+output:3
+"""
